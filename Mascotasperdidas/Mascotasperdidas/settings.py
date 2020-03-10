7eacac55e43172d3143824fb8e28d384302b66d5
Mateo Álvarez <alvarez.fing@gmail.com>
2020-03-10 20:14:27 -0300

    Implementar bootstrap en los formularios
    Es un framework CSS para editar contenido del proyecto de manera simple.

--- Mascotasperdidas/Mascotasperdidas/settings.py
+++ Mascotasperdidas/Mascotasperdidas/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Mascotas.apps.MascotasConfig'  # <---- manera correcta de introducir la app
+    'Mascotas.apps.MascotasConfig',  # <---- manera correcta de introducir la app
+    'crispy_forms',  #<---- para tomar los furmularios y aplicarles bootstrap 
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Mascotasperdidas/Mascotasperdidas/urls.py
+++ Mascotasperdidas/Mascotasperdidas/urls.py
@@ -1,18 +1,4 @@
-"""Mascotasperdidas URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from .views import home, nuevo_ingreso_perdido, nuevo_ingreso_encontrado, Listado_publicaciones, Modificar_publicaciones, Eliminar_publicacion, Ver_publicacion, registro_usuario
@@ -35,7 +21,7 @@ urlpatterns = [
     path('modificar_publicaciones/<id>/', Modificar_publicaciones, name='modificar_publicaciones'),
     path('eliminar_publicacion/<id>/', Eliminar_publicacion, name='eliminar_publicacion'),
     path('ver_publicacion/<id>/', Ver_publicacion, name='ver_publicacion'),
-    path('registro', registro_usuario, name='registro_usuario'),
+    path('registro/', registro_usuario, name='registro_usuario'),
 ]
 
 
